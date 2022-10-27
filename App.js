bb40ee472927e92ebc28536c31f4f0df55b5bdce
Jawad Bhimani <jawadbhimanii@gmail.com>
2022-10-26 22:48:42 -0400

    eslint

--- App.js
+++ App.js
@@ -7,13 +7,8 @@
  */
 
 import React from 'react';
-import Home from './src/screens/Home'
-
-
+import Home from './src/screens/Home';
 
 export default function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
-
